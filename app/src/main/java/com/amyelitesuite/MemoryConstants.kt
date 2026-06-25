446f9f7860a51bc3df8db643630a052b943a59dc
suhaimitoamy <34860608+suhaimitoamy@users.noreply.github.com>
2026-06-25 12:57:42 +0800

    fix(android): make trim memory constant runtime-safe

--- app/src/main/java/com/amyelitesuite/MemoryConstants.kt
+++ app/src/main/java/com/amyelitesuite/MemoryConstants.kt
@@ -2,4 +2,5 @@ package com.amyelitesuite
 
 import android.content.ComponentCallbacks2
 
-const val TRIM_MEMORY_RUNNING_LOW: Int = ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW
+val TRIM_MEMORY_RUNNING_LOW: Int
+    get() = ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW
