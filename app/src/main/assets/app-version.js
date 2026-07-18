85707c0c84cc43f9242385975a9c02d5b08f6c61
suhaimitoamy <34860608+suhaimitoamy@users.noreply.github.com>
2026-07-18 15:23:01 +0800

    release: bump Amy FX to 1.4.14

--- app/src/main/assets/app-version.js
+++ app/src/main/assets/app-version.js
@@ -1,5 +1,5 @@
 (function () {
-  const VERSION = Object.freeze({ name: '1.4.13', code: 36 });
+  const VERSION = Object.freeze({ name: '1.4.14', code: 37 });
   window.AmyFXAppVersion = VERSION;
 
   function versionText() {
