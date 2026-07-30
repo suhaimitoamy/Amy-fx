fa8c27289a8a487ddd2cd56ca0cb17d8a3f9f31e
suhaimitoamy <34860608+suhaimitoamy@users.noreply.github.com>
2026-07-30 23:35:41 +0800

    release(preview): publish web identity for preview 294 [skip ci]

--- app/src/main/assets/app-version.js
+++ app/src/main/assets/app-version.js
@@ -1,6 +1,6 @@
 // Amy FX Preview personal release identity.
 (function () {
-  const VERSION = Object.freeze({ name: '2.0.0-preview.293', code: 940293 });
+  const VERSION = Object.freeze({ name: '2.0.0-preview.294', code: 940294 });
   window.AmyFXAppVersion = VERSION;
   window.AmyFXUpdateManifestUrl = 'https://raw.githubusercontent.com/suhaimitoamy/Amy-fx/personal/amyfx-private/preview-update.json';
 
