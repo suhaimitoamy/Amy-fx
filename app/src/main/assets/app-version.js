583e56d48d666334d608a6e6254d4991b33ad303
suhaimitoamy <34860608+suhaimitoamy@users.noreply.github.com>
2026-08-02 08:42:10 +0800

    release: set Amy FX public version to 2.2.0

--- app/src/main/assets/app-version.js
+++ app/src/main/assets/app-version.js
@@ -1,6 +1,6 @@
 // Amy FX production release identity.
 (function () {
-  const VERSION = Object.freeze({ name: '2.1.1', code: 55 });
+  const VERSION = Object.freeze({ name: '2.2.0', code: 56 });
   window.AmyFXAppVersion = VERSION;
   window.AmyFXUpdateManifestUrl = 'https://raw.githubusercontent.com/suhaimitoamy/Amy-fx/main/update.json';
 
