d20de112548dd8bcfba9e8299e6fd8fe9c8cc851
suhaimitoamy <34860608+suhaimitoamy@users.noreply.github.com>
2026-08-03 13:25:14 +0800

    release(preview): set app identity to preview.303

--- app/src/main/assets/app-version.js
+++ app/src/main/assets/app-version.js
@@ -1,6 +1,6 @@
 // Amy FX Preview personal release identity.
 (function () {
-  const VERSION = Object.freeze({ name: '2.0.0-preview.302', code: 940302 });
+  const VERSION = Object.freeze({ name: '2.0.0-preview.303', code: 940303 });
   window.AmyFXAppVersion = VERSION;
   window.AmyFXUpdateManifestUrl = 'https://raw.githubusercontent.com/suhaimitoamy/Amy-fx/personal/amyfx-private/preview-update.json';
   function displayVersionName(name) {
